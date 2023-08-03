8984db4f497e9341e8bd55467cadd1c0518917e9
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-08-03 09:36:36 +0200

    Add themes

---
+++ src/components/theme/Theme.js
@@ -0,0 +1,50 @@
+import { amber, deepOrange, grey } from '@mui/material/colors';
+
+const theme = {
+  palette: {
+    primary: amber,
+  },
+};
+
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+      ? {
+          // palette values for light mode
+          primary: amber,
+          // divider: amber[200],
+          background: {
+            default: '#fff',
+          },
+          text: {
+            primary: '#000',
+            secondary: grey[800],
+          },
+        }
+      : {
+          // palette values for dark mode
+          primary: deepOrange,
+          divider: '#dddddd',
+          background: {
+            default: '#000',
+            paper: deepOrange[900],
+          },
+          text: {
+            primary: '#fff',
+            secondary: grey[500],
+          },
+        }),
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+    },
+  },
+});
+
+export { theme, getDesignTokens };
